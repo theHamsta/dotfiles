6a1ec4c79950051809a7abea73d450446965fd14
Stephan Seitz <stephan.seitz@fau.de>
2021-07-15 21:37:41 +0200

    Add zen-mode

--- .config/nvim/packages.lua
+++ .config/nvim/packages.lua
@@ -45,6 +45,16 @@ return packer.startup(function()
     end,
   }
   use {
+    "folke/zen-mode.nvim",
+    config = function()
+      require("zen-mode").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end,
+  }
+  use {
     "vhyrro/neorg",
     branch = "unstable",
     config = function()
