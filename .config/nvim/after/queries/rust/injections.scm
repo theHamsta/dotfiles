ccbf00a12cffb27360c9956d2c04b23ba5679d9d
Stephan Seitz <stephan.seitz@fau.de>
2021-11-30 21:31:43 +0100

    Add rust regex highlighting

---
+++ .config/nvim/after/queries/rust/injections.scm
@@ -0,0 +1,13 @@
+(call_expression
+  function: (scoped_identifier
+    path: (identifier) @_regex (#eq? @_regex "Regex")
+    name: (identifier) @_new (#eq? @_new "new"))
+  arguments: (arguments
+    (raw_string_literal) @regex))
+
+(call_expression
+  function: (scoped_identifier
+    path: (scoped_identifier (identifier) @_regex (#eq? @_regex "Regex").)
+    name: (identifier) @_new (#eq? @_new "new"))
+  arguments: (arguments
+    (raw_string_literal) @regex))
