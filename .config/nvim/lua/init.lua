2e974740309625ecbca45e78958ef362b0fa0717
Stephan Seitz <stephan.seitz@fau.de>
2022-04-28 21:57:59 +0200

    Use lua-language-server from brew

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -198,11 +198,8 @@ if ok then
       },
     }
   end
-  local sumneko_root_path = vim.fn.expand "~/projects/lua-language-server"
-  local sumneko_binary = sumneko_root_path .. "/bin/Linux/lua-language-server"
 
   lspconfig.sumneko_lua.setup {
-    cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
     settings = {
       Lua = {
         awakened = { cat = true },
