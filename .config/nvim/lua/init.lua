239b00ef267e6cbcd4d207f83b98fad9e7ed6e66
Stephan Seitz <stephan.seitz@fau.de>
2022-12-22 00:22:21 +0100

    Setup neodev

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -1,6 +1,6 @@
 local shell = require "nvim-treesitter.shell_command_selectors"
 
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/opt/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
   vim.cmd [[packadd packer.nvim]]
