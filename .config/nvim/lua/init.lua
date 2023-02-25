fa49cfe5fa3045ad310b4655c311738b86a58d11
Stephan Seitz <stephan.seitz@fau.de>
2023-02-25 22:11:59 +0100

    Update config

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -963,6 +963,7 @@ if ok then
     },
     indent = {
       enable = true,
+      disable = "python",
     },
     --ensure_installed = "all",
     --update_strategy = "do not use lockfile, please!"
