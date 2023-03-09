47b494fedce2c96ca930f94cf793d32a7ddf8922
Stephan Seitz <sseitz@nvidia.com>
2023-03-09 10:52:45 +0100

    Update config

--- .config/nvim/packages.lua
+++ .config/nvim/packages.lua
@@ -301,9 +301,29 @@ nnoremap <silent> <leader>gt  :lua require'agitator'.open_file_git_branch()<cr>
     keys = "s",
     config = function()
       require("leap").add_default_mappings()
-      vim.keymap.del({'n', 'x', 'o'}, 'x')
+      vim.keymap.del({ "n", "x", "o" }, "x")
     end,
   },
+  --{
+  --"ggandor/leap-spooky.nvim",
+  --config = function()
+  --require("leap-spooky").setup {
+  --affixes = {
+  ---- These will generate mappings for all native text objects, like:
+  ---- (ir|ar|iR|aR|im|am|iM|aM){obj}.
+  ---- Special line objects will also be added, by repeating the affixes.
+  ---- E.g. `yrr<leap>` and `ymm<leap>` will yank a line in the current
+  ---- window.
+  ---- You can also use 'rest' & 'move' as mnemonics.
+  --remote = { window = "r", cross_window = "R" },
+  --magnetic = { window = "m", cross_window = "M" },
+  --},
+  ---- If this option is set to true, the yanked text will automatically be pasted
+  ---- at the cursor position if the unnamed register is in use.
+  --paste_on_remote_yank = false,
+  --}
+  --end,
+  --},
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v2.x",
