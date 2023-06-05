7955d978ab8022db738d177f23d3c9bdf316bf8b
Stephan Seitz <sseitz@nvidia.com>
2023-06-05 10:36:48 +0200

    Update config

--- .config/nvim/packages.lua
+++ .config/nvim/packages.lua
@@ -76,7 +76,24 @@ require("lazy").setup {
   {
     "stevearc/oil.nvim",
     config = function()
-      require("oil").setup()
+      require("oil").setup {
+        use_default_keymaps = false,
+        keymaps = {
+          ["g?"] = "actions.show_help",
+          ["<CR>"] = "actions.select",
+          ["<C-s>"] = "actions.select_vsplit",
+          ["<C-h>"] = "actions.select_split",
+          ["<C-t>"] = "actions.select_tab",
+          ["<leader>p"] = "actions.preview",
+          ["<C-c>"] = "actions.close",
+          ["<C-l>"] = "actions.refresh",
+          ["-"] = "actions.parent",
+          ["_"] = "actions.open_cwd",
+          ["`"] = "actions.cd",
+          ["~"] = "actions.tcd",
+          ["g."] = "actions.toggle_hidden",
+        },
+      }
       vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
     end,
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -287,7 +304,7 @@ nnoremap <silent> <leader>gt  :lua require'agitator'.open_file_git_branch()<cr>
     config = function()
       require("fidget").setup {}
     end,
-    lazy = true,
+    lazy = false,
   },
   { "mattboehm/vim-unstack", cmd = "UnstackFromText" },
   {
