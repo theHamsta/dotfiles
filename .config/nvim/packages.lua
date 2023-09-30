a79979916258044a35b02978ad552e81cdb0d95c
Stephan Seitz <stephan.seitz@fau.de>
2023-09-30 23:14:16 +0200

    Use conform.nvim

--- .config/nvim/packages.lua
+++ .config/nvim/packages.lua
@@ -40,6 +40,26 @@ local go_packages = {
 
 require("lazy").setup {
   {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup {
+        formatters_by_ft = {
+          lua = { "stylua" },
+          ---- Conform will run multiple formatters sequentially
+          --python = { "isort", "black" },
+          ---- Use a sub-list to run only the first available formatter
+          javascript = { { "prettierd", "prettier" } },
+        },
+        format_on_save = {
+          -- I recommend these options. See :help conform.format for details.
+          lsp_fallback = true,
+          timeout_ms = 500,
+        },
+      }
+    end,
+    event = "VeryLazy",
+  },
+  {
     "RaafatTurki/hex.nvim",
     config = function()
       require("hex").setup()
