13ed258e65e59956884e73020b1cf7bf65755a3e
Stephan Seitz <stephan.seitz@fau.de>
2023-11-26 19:10:09 +0100

    Update config

--- .config/nvim/packages.lua
+++ .config/nvim/packages.lua
@@ -49,6 +49,7 @@ require("lazy").setup {
           --python = { "isort", "black" },
           ---- Use a sub-list to run only the first available formatter
           javascript = { { "prettierd", "prettier" } },
+          ["_"] = { "trim_whitespace" },
         },
         --format_on_save = {
         ---- I recommend these options. See :help conform.format for details.
