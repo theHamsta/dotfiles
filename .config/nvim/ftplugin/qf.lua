223332f43dab4aa1b945dbc1946edf400462b80f
Stephan Seitz <stephan.seitz@fau.de>
2024-01-05 22:04:51 +0100

    Fix replacement mapping

--- .config/nvim/ftplugin/qf.lua
+++ .config/nvim/ftplugin/qf.lua
@@ -1,2 +1,7 @@
-vim.keymap.set("n", "R", ":cdo s//g<left><left>", { silent = true, buffer = true, noremap = true })
-vim.keymap.set("n", "L", ":ldo s//g<left><left>", { silent = true, buffer = true, noremap = true })
+vim.keymap.set("n", "R", function()
+  if vim.fn.getloclist(0, { winid = 0 }).winid == 0 then
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(":cdo s//g<left><left>", true, false, true), "n", false)
+  else
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(":ldo s//g<left><left>", true, false, true), "n", false)
+  end
+end, { silent = true, buffer = true, noremap = true })
