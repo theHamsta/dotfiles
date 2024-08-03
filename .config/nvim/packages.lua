eccc9b3c84645e06a5548105994df8ccb6797999
Stephan Seitz <stephan.seitz@fau.de>
2024-08-03 15:23:52 +0200

    Add spectre

--- .config/nvim/packages.lua
+++ .config/nvim/packages.lua
@@ -150,6 +150,24 @@ require("lazy").setup {
     ft = { "javascript", "typescript" },
   },
   {
+    "nvim-pack/nvim-spectre",
+    config = function()
+      require("spectre").setup()
+      vim.keymap.set("n", "<leader>Ü", '<cmd>lua require("spectre").toggle()<CR>', {
+        desc = "Toggle Spectre",
+      })
+      vim.keymap.set("n", "<leader>üw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+        desc = "Search current word",
+      })
+      vim.keymap.set("v", "<leader>üw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+        desc = "Search current word",
+      })
+      vim.keymap.set("n", "<leader>üp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+        desc = "Search on current file",
+      })
+    end,
+  },
+  {
     "liangxianzhe/nap.nvim",
     config = function()
       require("nap").setup {
