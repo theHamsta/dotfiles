9deffbebb7a8a7c78860eb8e90b0faf8330090f0
Stephan Seitz <stephan.seitz@fau.de>
2025-03-15 10:41:06 +0100

    Enable devcontainers

--- .config/nvim/lua/packages.lua
+++ .config/nvim/lua/packages.lua
@@ -489,74 +489,75 @@ require("lazy").setup {
   --end,
   --},
   --
-  --"esensar/nvim-dev-container",
-  --config = function()
-  --require("devcontainer").setup {
-  --generate_commands = true,
-  ---- By default no autocommands are generated
-  ---- This option can be used to configure automatic starting and cleaning of containers
-  --autocommands = {
-  ---- can be set to true to automatically start containers when devcontainer.json is available
-  --init = false,
-  ---- can be set to true to automatically remove any started containers and any built images when exiting vim
-  --clean = false,
-  ---- can be set to true to automatically restart containers when devcontainer.json file is updated
-  --update = true,
-  --},
-  ---- can be changed to increase or decrease logging from library
-  --log_level = "info",
-  ---- can be set to true to disable recursive search
-  ---- in that case only .devcontainer.json and .devcontainer/devcontainer.json files will be checked relative
-  ---- to the directory provided by config_search_start
-  --disable_recursive_config_search = false,
-  ---- By default all mounts are added (config, data and state)
-  ---- This can be changed to disable mounts or change their options
-  ---- This can be useful to mount local configuration
-  ---- And any other mounts when attaching to containers with this plugin
-  --attach_mounts = {
-  ---- Can be set to true to always mount items defined below
-  ---- And not only when directly attaching
-  ---- This can be useful if executing attach command separately
-  --always = true,
-  --neovim_config = {
-  ---- enables mounting local config to /root/.config/nvim in container
-  --enabled = true,
-  ---- makes mount readonly in container
-  --options = { "readonly" },
-  --},
-  --neovim_data = {
-  ---- enables mounting local data to /root/.local/share/nvim in container
-  --enabled = false,
-  ---- no options by default
-  --options = {},
-  --},
-  ---- Only useful if using neovim 0.8.0+
-  --neovim_state = {
-  ---- enables mounting local state to /root/.local/state/nvim in container
-  --enabled = false,
-  ---- no options by default
-  --options = {},
-  --},
-  ---- This takes a list of mounts (strings) that should always be added whenever attaching to containers
-  ---- This is passed directly as --mount option to docker command
-  ---- Or multiple --mount options if there are multiple values
-  --custom_mounts = {},
-  --},
-  ---- This takes a list of mounts (strings) that should always be added to every run container
-  ---- This is passed directly as --mount option to docker command
-  ---- Or multiple --mount options if there are multiple values
-  --always_mount = {},
-  ---- This takes a string (usually either "podman" or "docker") representing container runtime
-  ---- That is the command that will be invoked for container operations
-  ---- If it is nil, plugin will use whatever is available (trying "podman" first)
-  --container_runtime = nil,
-  ---- This takes a string (usually either "podman-compose" or "docker-compose") representing compose command
-  ---- That is the command that will be invoked for compose operations
-  ---- If it is nil, plugin will use whatever is available (trying "podman-compose" first)
-  --compose_command = nil,
-  --}
-  --end,
-  --},
+  {
+    "esensar/nvim-dev-container",
+    config = function()
+      require("devcontainer").setup {
+        generate_commands = true,
+        -- By default no autocommands are generated
+        -- This option can be used to configure automatic starting and cleaning of containers
+        autocommands = {
+          -- can be set to true to automatically start containers when devcontainer.json is available
+          init = false,
+          -- can be set to true to automatically remove any started containers and any built images when exiting vim
+          clean = false,
+          -- can be set to true to automatically restart containers when devcontainer.json file is updated
+          update = true,
+        },
+        -- can be changed to increase or decrease logging from library
+        log_level = "info",
+        -- can be set to true to disable recursive search
+        -- in that case only .devcontainer.json and .devcontainer/devcontainer.json files will be checked relative
+        -- to the directory provided by config_search_start
+        disable_recursive_config_search = false,
+        -- By default all mounts are added (config, data and state)
+        -- This can be changed to disable mounts or change their options
+        -- This can be useful to mount local configuration
+        -- And any other mounts when attaching to containers with this plugin
+        attach_mounts = {
+          -- Can be set to true to always mount items defined below
+          -- And not only when directly attaching
+          -- This can be useful if executing attach command separately
+          always = true,
+          neovim_config = {
+            -- enables mounting local config to /root/.config/nvim in container
+            enabled = true,
+            -- makes mount readonly in container
+            options = { "readonly" },
+          },
+          neovim_data = {
+            -- enables mounting local data to /root/.local/share/nvim in container
+            enabled = false,
+            -- no options by default
+            options = {},
+          },
+          -- Only useful if using neovim 0.8.0+
+          neovim_state = {
+            -- enables mounting local state to /root/.local/state/nvim in container
+            enabled = false,
+            -- no options by default
+            options = {},
+          },
+          -- This takes a list of mounts (strings) that should always be added whenever attaching to containers
+          -- This is passed directly as --mount option to docker command
+          -- Or multiple --mount options if there are multiple values
+          custom_mounts = {},
+        },
+        -- This takes a list of mounts (strings) that should always be added to every run container
+        -- This is passed directly as --mount option to docker command
+        -- Or multiple --mount options if there are multiple values
+        always_mount = {},
+        -- This takes a string (usually either "podman" or "docker") representing container runtime
+        -- That is the command that will be invoked for container operations
+        -- If it is nil, plugin will use whatever is available (trying "podman" first)
+        container_runtime = nil,
+        -- This takes a string (usually either "podman-compose" or "docker-compose") representing compose command
+        -- That is the command that will be invoked for compose operations
+        -- If it is nil, plugin will use whatever is available (trying "podman-compose" first)
+        compose_command = nil,
+      }
+    end,
+  },
   --{
   --"ggandor/leap-spooky.nvim",
   --config = function()
