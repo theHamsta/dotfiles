c2cd38081e1a0566640284083430f4ce661d22f8
Stephan Seitz <stephan.seitz@fau.de>
2025-07-31 00:12:20 +0200

    Add spirv ftplugin

---
+++ .config/nvim/ftplugin/spirv.lua
@@ -0,0 +1,9 @@
+#! /usr/bin/env lua
+--
+-- spirv.lua
+-- Copyright (C) 2025 stephan <stephan@stephan-nvidia-workstation>
+--
+-- Distributed under terms of the GPLv3 license.
+--
+
+vim.fn.NvimLspMaps()
