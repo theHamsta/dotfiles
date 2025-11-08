69c7706d3993cf7bfe13be02f66347ba12f6e432
Stephan Seitz <stephan.seitz@fau.de>
2025-11-08 12:31:43 +0100

    Add oil.nvim

--- .config/nvim/lua/packages.lua
+++ .config/nvim/lua/packages.lua
@@ -52,6 +52,17 @@ require("lazy").setup {
         lazy = false,
     },
     {
+        "stevearc/oil.nvim",
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {},
+        -- Optional dependencies
+        dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+        -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+        -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+        lazy = false,
+    },
+    {
         "kevinhwang91/nvim-ufo",
         dependencies = "kevinhwang91/promise-async",
         config = function()
