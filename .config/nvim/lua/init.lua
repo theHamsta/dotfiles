30848f02a2eaeb0fa0fbfded9f23ef7059b8d526
Stephan Seitz <stephan.seitz@fau.de>
2026-03-30 22:52:28 +0200

    Try out codelens

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -294,6 +294,7 @@ local function on_attach(client, bufnr)
     if lsp_status then
         lsp_status.on_attach(client)
     end
+    vim.lsp.codelens.enable()
 end
 
 local function lsp_setup(name, config, disable)
