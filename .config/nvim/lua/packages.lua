cc3955f1ca7b23b8d744f42b9ae73d7df3fbad42
Stephan Seitz <stephan.seitz@fau.de>
2026-09-07 20:13:25 +0200

    CSV tools

--- .config/nvim/lua/packages.lua
+++ .config/nvim/lua/packages.lua
@@ -102,6 +102,19 @@ require("lazy").setup {
         lazy = false,
     },
     {
+        "Decodetalkers/csv-tools.lua",
+        config = {
+            before = 10,
+            after = 10,
+            clearafter = true,
+            -- this will clear the highlight of buf after move
+            showoverflow = true,
+            -- this will provide a overflow show
+            titelflow = true,
+            -- add an alone title
+        },
+    },
+    {
         "folke/snacks.nvim",
         ---@type snacks.Config
         opts = {
